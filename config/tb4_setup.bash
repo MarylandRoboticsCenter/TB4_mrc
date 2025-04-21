a6fb0ec3569a026f1b2381a3950ba096d876859e
Ivan Penskiy <ipenskiy@umd.edu>
2025-04-21 09:37:30 -0400

    small fixes

--- config/tb4_setup.bash
+++ config/tb4_setup.bash
@@ -1,9 +1,15 @@
 export ROS_DOMAIN_ID=0
-# export ROS_LOCALHOST_ONLY=0
 export ROS_AUTOMATIC_DISCOVERY_RANGE=SUBNET
 export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
 
+# !!!important!!! for TB4_1
 export ROS_SUPER_CLIENT=True
-export ROS_DISCOVERY_SERVER="192.168.79.215:11811;"
+export ROS_DISCOVERY_SERVER=";192.168.79.228:11811;"
 
+# # !!!important!!! for TB4_2
+# export ROS_SUPER_CLIENT=True
+# export ROS_DISCOVERY_SERVER=";192.168.79.215:11888;"
+
+# old or not used yet
 # export FASTRTPS_DEFAULT_PROFILES_FILE=$HOME/${WS_DIR}/config/fastdds_mrc.xml
+# export ROS_LOCALHOST_ONLY=0
