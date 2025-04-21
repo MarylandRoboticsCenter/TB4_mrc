d4fb300859f9230e8c723859e25fa0ae78da7038
Ivan Penskiy <ipenskiy@umd.edu>
2025-04-21 09:38:01 -0400

    fixed typo

--- config/tb4_setup.bash
+++ config/tb4_setup.bash
@@ -1,13 +1,12 @@
 export ROS_DOMAIN_ID=0
 export ROS_AUTOMATIC_DISCOVERY_RANGE=SUBNET
 export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+export ROS_SUPER_CLIENT=True
 
 # !!!important!!! for TB4_1
-export ROS_SUPER_CLIENT=True
 export ROS_DISCOVERY_SERVER=";192.168.79.228:11811;"
 
 # # !!!important!!! for TB4_2
-# export ROS_SUPER_CLIENT=True
 # export ROS_DISCOVERY_SERVER=";192.168.79.215:11888;"
 
 # old or not used yet
